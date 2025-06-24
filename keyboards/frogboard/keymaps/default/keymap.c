834dd3b7f2bf0dfc0acd4834ede99e7592234770
LucasWithK <176577210+LucasWithK@users.noreply.github.com>
2025-06-24 09:08:25 +0200

    qwerty

--- keyboards/frogboard/keymaps/default/keymap.c
+++ keyboards/frogboard/keymaps/default/keymap.c
@@ -3,54 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-enum unicode_names {
-    ae,
-    AE,
-    oe,
-    OE,
-    ue,
-    UE,
-    SS,
-    EU
-};
-
-const uint32_t PROGMEM unicode_map[] = {
-    [ae] = 0x00E4,  // ä
-    [AE] = 0x00C4,  // Ä
-    [oe] = 0x00F6,  // ö
-    [OE] = 0x00D6,  // Ö
-    [ue] = 0x00FC,  // ü
-    [UE] = 0x00DC,  // Ü
-    [SS] = 0x00DF,  // ß
-    [EU] = 0x20AC,  // €
-};
-
-enum custom_keycodes {
-    MY_UC_NEXT = SAFE_RANGE,
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-    case MY_UC_NEXT:
-        if (record->event.pressed) {;
-            unicode_input_mode_step();
-            switch (get_unicode_input_mode()) {
-                case UNICODE_MODE_LINUX:
-                    SEND_STRING("UNICODE_MODE_LINUX");
-                    break;
-                case UNICODE_MODE_MACOS:
-                    SEND_STRING("UNICODE_MODE_MACOS");
-                    break;
-                case UNICODE_MODE_WINCOMPOSE:
-                    SEND_STRING("UNICODE_MODE_WINCOMPOSE");
-                    break;
-            };
-        }
-        break;
-    }
-    return true;
-};
-
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
   debug_enable=true;
@@ -65,14 +17,14 @@ void keyboard_post_init_user(void) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-        KC_Q,               KC_W,               KC_F,               KC_P,               KC_B,
-        KC_J,               KC_L,               KC_U,               KC_Y,               KC_SCLN,
+        KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,
+        KC_Y,               KC_U,               KC_I,               KC_O,               KC_P,
 
-        KC_A,               KC_R,               KC_S,               KC_T,               KC_G,
-        KC_M,               KC_N,               KC_E,               KC_I,               KC_O,
+        LSFT_T(KC_A),       KC_S,               KC_D,               KC_F,               KC_G,
+        KC_H,               KC_J,               KC_K,               KC_L,               RSFT_T(KC_SCLN),
 
-        GUI_T(KC_Z),        ALT_T(KC_X),        CTL_T(KC_C),       LSFT_T(KC_D),       KC_V,
-        KC_K,               RSFT_T(KC_H),       CTL_T(KC_COMM),    ALT_T(KC_DOT),      GUI_T(KC_SLSH),
+        CTL_T(KC_Z),        GUI_T(KC_X),        ALT_T(KC_C),        KC_V,               KC_B,
+        KC_N,               KC_M,               ALT_T(KC_COMM),     GUI_T(KC_DOT),      CTL_T(KC_SLSH),
 
         KC_ESC,             KC_BSPC,            KC_DEL,
         LT(OTHR, KC_ENT),   LT(MAIN, KC_SPC),   KC_TAB
@@ -82,31 +34,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1,               KC_2,               KC_3,               KC_4,               KC_5,
         KC_6,               KC_7,               KC_8,               KC_9,               KC_0,
 
-        KC_GRV,             KC_EQL,             KC_MINS,            KC_QUOT,            KC_NO,
-        KC_NO,              KC_LEFT,            KC_DOWN,            KC_UP,              KC_RIGHT,
+        LSFT_T(KC_GRV),     KC_NO,              KC_EQL,             KC_MINS,            KC_F11,
+        KC_F12,             KC_LBRC,            KC_RBRC,            KC_BSLS,            RSFT_T(KC_QUOT),
 
-        GUI_T(KC_NO),       ALT_T(KC_NO),       CTL_T(KC_NO),       LSFT_T(KC_NO),      KC_NO,
-        KC_NO,              RSFT_T(KC_LBRC),    CTL_T(KC_RBRC),     ALT_T(KC_BSLS),     GUI_T(KC_NO),
+        CTL_T(KC_F1),       GUI_T(KC_F2),       ALT_T(KC_F3),       KC_F4,              KC_F5,
+        KC_F6,              KC_F7,              ALT_T(KC_F8),       GUI_T(KC_F9),       CTL_T(KC_F10),
 
         KC_TRNS,            KC_TRNS,            KC_TRNS,
         KC_TRNS,            KC_TRNS,            KC_TRNS
     ),
 
     [OTHR] = LAYOUT(
-        KC_F1,              KC_F2,              KC_F3,              KC_F4,              KC_F5,
-        KC_F6,              KC_F7,              KC_F8,              KC_F9,              KC_F10,
+        ALGR(KC_Q),         KC_NO,              KC_NO,              KC_NO,              ALGR(KC_5),
+        ALGR(KC_Y),         KC_NO,              KC_NO,              KC_NO,              ALGR(KC_P),
 
-        UM(SS),             UP(ue, UE),         UP(oe, OE),         UP(ae, AE),         UM(EU),
-        KC_F11,             KC_F12,             KC_NO,              KC_NO,              KC_NO,
+        LSFT_T(KC_NO),      ALGR(KC_S),         KC_NO,              KC_NO,              KC_NO,
+        KC_LEFT,            KC_DOWN,            KC_UP,              KC_RIGHT,           RSFT_T(KC_NO),
 
-        GUI_T(KC_CAPS),     ALT_T(KC_INS),      CTL_T(KC_HOME),     LSFT_T(KC_END),     MY_UC_NEXT,
-        KC_PSCR,            RSFT_T(KC_PAUS),    CTL_T(KC_PGDN),     ALT_T(KC_PGUP),     GUI_T(KC_NUM),
+        CTL_T(KC_CAPS),     GUI_T(KC_INS),      ALT_T(KC_HOME),     KC_END,             KC_NO,
+        KC_PSCR,            KC_PGDN,            ALT_T(KC_PGUP),     GUI_T(KC_PAUS),     CTL_T(KC_SCRL),
 
         KC_TRNS,            KC_TRNS,            KC_TRNS,
         KC_TRNS,            KC_TRNS,            KC_TRNS
     ),
 };
-// maybe move € next no ' on MAIN, F11 and F12 left, put mouse keys on the righ home row in OTHER
-// and find a position for left, middle, and right click, and maybe scrolling.
-// maybe by moving PDWN, PGUP, INS, HOME, END, to, MAIN layer so the KC_NOs get filled.
-// or find often used combos that can be put there, but maybe that does not work with the tap hold keys.
